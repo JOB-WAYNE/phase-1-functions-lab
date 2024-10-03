a0231453a0dd10b1b4916329503629be16348064
jobwayne <otienojobwayneotieno@gmail.com>
2024-10-03 16:18:21 +0300

    functionlab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (pickupLocation){ 
+        const Hqlocation = 42;      
+return  Math.abs( pickupLocation - Hqlocation); 
+}
+function distanceFromHqInFeet(pickupLocation) {
+         const blocks=distanceFromHqInBlocks(pickupLocation)
+         return blocks * 264 
+        }
+function distanceTravelledInFeet(start, destination) {
+         return Math.abs (destination-start)*264
+}
+function calculatesFarePrice(start, destination){
+          let   distance = distanceTravelledInFeet(start, destination)
+        if (distance < 400)
+         {return 0;}
+        else if (distance >= 400 && distance <=2000 )
+         {return (distance-400)*0.02;}
+        else if (distance >2000 && distance < 2500)
+         {return 25;}
+        else (distance >2500)
+         {return  'cannot travel that far';}
+        
+}    
+         
+      
+        
+        
+        
+        
+        
+        
+        
+
